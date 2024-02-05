f5c7d4a79dc58c2488878b24468dc7e18a688e06
Amir Ali <amiraliazimloo123@gmail.com>
2024-02-05 21:45:20 +0330

    reusable & powerfull drop down completed

--- src/components/DGXDropdown.js
+++ src/components/DGXDropdown.js
@@ -50,7 +50,10 @@ const DGXDropdown = ({ options, onSelect, placeHolder, renderOption }) => {
         )}
       </div>
       {/* overlay */}
-      <div className={`${isOpen ? "overlay" : "hidden"}`} onClick={toggleDropdown} />
+      <div
+        className={`${isOpen ? "overlay" : "hidden"}`}
+        onClick={toggleDropdown}
+      />
     </>
   );
 };
